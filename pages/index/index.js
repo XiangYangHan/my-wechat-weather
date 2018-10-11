6f98fb5775bf55136db7b2160058356f0ba9767a
hans <hans_2015@yeah.net>
2018-10-11 22:17:44 +0800

    get from weather API

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,11 @@
 Page({
   onLoad() {
     console.log('Hello World!');
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now?city=上海市',
+      data: {
+      }, 
+      success: res => console.log(res)
+    })
   }
 })
