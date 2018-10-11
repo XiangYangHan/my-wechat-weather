5bef9b11dddf1c4e0368f973c8b036adec56610e
hans <hans_2015@yeah.net>
2018-10-11 22:26:12 +0800

    get temp and weather from data

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,12 @@ Page({
       url: 'https://test-miniprogram.com/api/weather/now?city=上海市',
       data: {
       }, 
-      success: res => console.log(res)
+      success: res => {
+        let result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        console.log(temp, weather);
+      }
     })
   }
 })
